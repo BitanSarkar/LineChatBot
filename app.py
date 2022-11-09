2222a3369a83225a4699e21fd468f07c2590d9c5
Bitan Sarkar <bitan.sarkar@publicissapient.com>
2022-11-09 09:05:12 +0530

    add'

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-import os
-import base64
-import hashlib
-import hmac
 from flask import Flask, request, abort
 
 from linebot import (
@@ -16,36 +12,35 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('AmWGLaN1W494BzGcEwrQ6rsYxHVvFdG/jx65lSlNb18H1IXcwSLsMMp1kLHPKScISdJbgat7FG7lpWHjJ34q4LZTYM1xtCFZcyp+EOn1O+ZkAjXubup19feb0VZIQ5XMPOWSU3Nv8h/Yrnc2aOKjcwdB04t89/1O/w1cDnyilFU=')
-channel_secret = '213b556081b80c24efef210f095697b8'
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # Get X-Line-Signature header value
-    signature2 = request.headers['x-line-signature']
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-    # Get request body as text
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # Handle webhook body
-    hash = hmac.new(channel_secret.encode('utf-8'),
-    body.encode('utf-8'), hashlib.sha256).digest()
-    signature1 = base64.b64encode(hash)
-    if signature2 != signature1:
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
+
     return 'OK'
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    """ Here's all the messages will be handled and processed by the program """
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
