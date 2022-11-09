66611e518e7d09ba84efa6a8508b174314b51515
Bitan Sarkar <bitan.sarkar@publicissapient.com>
2022-11-09 09:58:03 +0530

    add'

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ import os
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ.get('LINE_CHANNEL_ACCESS_TOKEN'))
-handler = WebhookHandler(os.environ.get('LINE_CHANNEL_SECRET'))
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 
 
 @app.route("/callback", methods=['POST'])
