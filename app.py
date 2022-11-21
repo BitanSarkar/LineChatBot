18d090a4fde4550d779f97303445888090c9b185
Bitan Sarkar <bitan.sarkar@publicissapient.com>
2022-11-21 17:01:39 +0530

    heell

--- app.py
+++ app.py
@@ -91,24 +91,27 @@ def handle_message(event):
                 "place": got_message,
                 "is_required": True
             }
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"When do you want to travel to {got_message}?"))
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"When do you want to travel to {got_message.capitalize()}?"))
         if last_message_info["is_required"] and last_message_info["message"] == "When do you want to travel?":
+            place = userData[user_id].place
             userData[user_id] = {
                 "message": "In which city are you right now?",
-                "place": userData[user_id].place,
+                "place": place,
                 "time": got_message,
                 "is_required": True
             }
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text="In which city are you right now?"))
         if last_message_info["is_required"] and last_message_info["message"] == "In which city are you right now?":
+            place = userData[user_id].place
+            time = userData[user_id].time
             userData[user_id] = {
                 "message": "In which city are you right now?",
-                "place": userData[user_id].place,
-                "time": userData[user_id].time,
+                "place": place,
+                "time": time,
                 "from_place": got_message,
                 "is_required": True
             }
-            line_bot_api.reply_message(event.reply_token, messages=[TextSendMessage(text=f"Searching for flights from {got_message} to {userData[user_id].place} on {userData[user_id].time}"), TextSendMessage(text=f"These are your options,\n1. Flight ABC\n2. Flight DEF\n3. Flight GFHI   \nSelect flight by clicking options [1 - 5]")])
+            line_bot_api.reply_message(event.reply_token, messages=[TextSendMessage(text=f"Searching for flights from {got_message.capitalize()} to {place.capitalize()} on {time.capitalize()}"), TextSendMessage(text=f"These are your options,\n1. Flight ABC\n2. Flight DEF\n3. Flight GFHI   \nSelect flight by clicking options [1 - 5]")])
     replyMessage = "Can't understand what you are trying to say! \U0001f615"
     if "hey" in got_message or "hello" in got_message  or "hi" in got_message :
         if "flight" in got_message or "search" in got_message:
