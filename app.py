f63e8aee843d15c396e517693a7ec14c288e3e4b
Bitan Sarkar <bitan.sarkar@publicissapient.com>
2022-11-21 16:51:24 +0530

    heell

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    got_message = event.message.text.lower()
+    got_message = event.message.text.lower().strip()
     print(event.source)
     user_id = event.source.user_id
     last_message_info = {}
@@ -86,25 +86,42 @@ def handle_message(event):
                 }
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Where do you want to travel?"))
         if last_message_info["is_required"] and last_message_info["message"] == "Where do you want to travel?":
-            if "yes" in got_message or "yup" in got_message:
-                userData[user_id] = {
-                    "message": "Where do you want to travel?",
-                    "is_required": True
-                }
-                line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Where do you want to travel?"))
+            userData[user_id] = {
+                "message": "When do you want to travel?",
+                "place": got_message,
+                "is_required": True
+            }
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"When do you want to travel to {got_message}?"))
+        if last_message_info["is_required"] and last_message_info["message"] == "When do you want to travel?":
+            userData[user_id] = {
+                "message": "In which city are you right now?",
+                "place": userData[user_id].place,
+                "time": got_message,
+                "is_required": True
+            }
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="In which city are you right now?"))
+        if last_message_info["is_required"] and last_message_info["message"] == "In which city are you right now?":
+            userData[user_id] = {
+                "message": "In which city are you right now?",
+                "place": userData[user_id].place,
+                "time": userData[user_id].time,
+                "from_place": got_message,
+                "is_required": True
+            }
+            line_bot_api.reply_message(event.reply_token, messages=[TextSendMessage(text=f"Searching for flights from {got_message} to {userData[user_id].place} on {userData[user_id].time}"), TextSendMessage(text=f"These are your options,\n1. Flight ABC\n2. Flight DEF\n3. Flight GFHI   \nSelect flight by clicking options [1 - 5]")])
     replyMessage = "Can't understand what you are trying to say! \U0001f615"
     if "hey" in got_message or "hello" in got_message  or "hi" in got_message :
         if "flight" in got_message or "search" in got_message:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="So you are search for flights?"))
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="So you want to search for flights?"))
         if "flight" in got_message or "search" in got_message:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="So you are search for flights?"))
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="So you want to search for flights?"))
         userData[user_id] = {
                 "message": "Do you wish to travel somewhere?",
                 "is_required": True
             }
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Do you wish to travel somewhere? \U0001f60d"))
     if "flight" in got_message or "search" in got_message:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="So you are search for flights?"))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="So you want to search for flights?"))
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=replyMessage))
 
 if __name__ == "__main__":
