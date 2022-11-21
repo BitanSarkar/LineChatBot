9ca9c104541c46fc834b4fdab268be4e0b56be08
Bitan Sarkar <bitan.sarkar@publicissapient.com>
2022-11-21 15:47:01 +0530

    heell

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ.get('LINE_CHANNEL_ACCESS_TOKEN', 'abc'))
 handler = WebhookHandler(os.environ.get('LINE_CHANNEL_SECRET', 'abc'))
 
+userData = {}
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -71,15 +72,36 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print(event.source)
     got_message = event.message.text.lower()
+    user_id = event.source.userId
+    last_message_info = {}
+    if user_id in userData:
+        last_message_info = userData[user_id]
+    if last_message_info["is_required"] and last_message_info["Do you wish to travel somewhere?"]:
+        if "yes" in got_message or "yup" in got_message:
+            userData[user_id] = {
+                "last_message_info": "Where do you want to travel?",
+                "is_required": True
+            }
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Where do you want to travel?"))
+    if last_message_info["is_required"] and last_message_info["Where do you want to travel?"]:
+        if "yes" in got_message or "yup" in got_message:
+            userData[user_id] = {
+                "last_message_info": "Where do you want to travel?",
+                "is_required": True
+            }
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Where do you want to travel?"))
     replyMessage = "Can't understand what you are trying to say! \U0001f615"
     if "hey" in got_message or "hello" in got_message  or "hi" in got_message :
         if "flight" in got_message or "search" in got_message:
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text="So you are search for flights?"))
         if "flight" in got_message or "search" in got_message:
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text="So you are search for flights?"))
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Do you wish to travel somewhere! \U0001f60d"))
+        userData[user_id] = {
+                "last_message_info": "Do you wish to travel somewhere?",
+                "is_required": True
+            }
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Do you wish to travel somewhere? \U0001f60d"))
     if "flight" in got_message or "search" in got_message:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text="So you are search for flights?"))
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=replyMessage))
