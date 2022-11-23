a9187ec2b8cc6e4e40bbdcfcb295e98331550a4b
Bitan Sarkar <bitan.sarkar@publicissapient.com>
2022-11-23 20:50:46 +0530

    s

--- app.py
+++ app.py
@@ -778,7 +778,7 @@ def handle_message(event):
     message_content = line_bot_api.get_message_content(event.message.id)
     print(message_content.content)
     with open(str(user_id)+".mp4", 'w') as wfile:
-        wfile.write(message_content.content)    
+        wfile.write(str(message_content.content))    
     wfile.close()
     sound = AudioSegment.from_file(str(user_id)+".mp4",format="mp4")
     sound.export(str(user_id)+".wav", format="wav")
